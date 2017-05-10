5587eb22751a7ea709e5ad8ecfb832f9e8f8d654
Dominic Carretto <trimoxal@gmail.com>
2017-05-10 08:20:19 -0400

    feat(menu): Initial commit for MDC menu foundation integration

---
+++ src/components/menu/index.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MenuComponent } from './menu';
+import { MenuItemDirective } from './menu-item';
+
+const MENU_COMPONENTS = [
+	MenuComponent,
+	MenuItemDirective
+];
+
+@NgModule({
+	imports: [CommonModule],
+	exports: [MENU_COMPONENTS],
+	declarations: [MENU_COMPONENTS],
+})
+export class MenuModule { }
---
+++ src/components/menu/menu-adapter.d.ts
@@ -0,0 +1,32 @@
+interface MDCMenuAdapter {
+	addClass: (string) => void
+	removeClass: (string) => void
+	hasClass: (string) => void
+	hasNecessaryDom: () => boolean
+	getInnerDimensions: () => { width: number, height: number }
+	hasAnchor: () => boolean
+	getAnchorDimensions: () => { width: number, height: number, top: number, right: number, bottom: number, left: number }
+	getWindowDimensions: () => { width: number, height: number }
+	setScale: (x: number, y: number) => void
+	setInnerScale: (x: number, y: number) => void
+	getNumberOfItems: () => number
+	registerInteractionHandler: (type: string, handler: EventListener) => void
+	deregisterInteractionHandler: (type: string, handler: EventListener) => void
+	registerDocumentClickHandler: (handler: EventListener) => void
+	deregisterDocumentClickHandler: (handler: EventListener) => void
+	getYParamsForItemAtIndex: (index: number) => { top: number, height: number }
+	setTransitionDelayForItemAtIndex: (index: number, value: string) => void
+	getIndexForEventTarget: (target: EventTarget) => number
+	notifySelected: (evtData: { index: number }) => void
+	notifyCancel: () => void
+	saveFocus: () => void
+	restoreFocus: () => void
+	isFocused: () => boolean
+	focus: () => void
+	getFocusedItemIndex: () => number
+	focusItemAtIndex: (index: number) => void
+	isRtl: () => boolean
+	setTransformOrigin: (origin: string) => void
+	setPosition: (position: { top: string, right: string, bottom: string, left: string }) => void
+	getAccurateTime: () => number
+}

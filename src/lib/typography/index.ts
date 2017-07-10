67517dd28cbf001dc1bf61ffe9c75b892aedfc75
Dominic Carretto <trimoxal@gmail.com>
2017-07-10 07:37:58 -0400

    fix(typography): Fix missing module import of [typography]

--- src/lib/typography/index.ts
+++ src/lib/typography/index.ts
@@ -1,37 +1,39 @@
 import { NgModule } from '@angular/core';
 
 import {
-	AdjustMarginDirective,
-	Display1Directive,
-	Display2Directive,
-	Display3Directive,
-	Display4Directive,
-	HeadlineDirective,
-	TitleDirective,
-	Subheading1Directive,
-	Subheading2Directive,
-	Body1Directive,
-	Body2Directive,
-	CaptionDirective
+  Typography,
+  AdjustMarginDirective,
+  Display1Directive,
+  Display2Directive,
+  Display3Directive,
+  Display4Directive,
+  HeadlineDirective,
+  TitleDirective,
+  Subheading1Directive,
+  Subheading2Directive,
+  Body1Directive,
+  Body2Directive,
+  CaptionDirective
 } from './typography.directive';
 
 const TYPOGRAPHY_DIRECTIVES = [
-	AdjustMarginDirective,
-	Display1Directive,
-	Display2Directive,
-	Display3Directive,
-	Display4Directive,
-	HeadlineDirective,
-	TitleDirective,
-	Subheading1Directive,
-	Subheading2Directive,
-	Body1Directive,
-	Body2Directive,
-	CaptionDirective
+  Typography,
+  AdjustMarginDirective,
+  Display1Directive,
+  Display2Directive,
+  Display3Directive,
+  Display4Directive,
+  HeadlineDirective,
+  TitleDirective,
+  Subheading1Directive,
+  Subheading2Directive,
+  Body1Directive,
+  Body2Directive,
+  CaptionDirective
 ];
 
 @NgModule({
-	exports: [TYPOGRAPHY_DIRECTIVES],
-	declarations: [TYPOGRAPHY_DIRECTIVES],
+  exports: [TYPOGRAPHY_DIRECTIVES],
+  declarations: [TYPOGRAPHY_DIRECTIVES],
 })
 export class TypographyModule { }

07ec20a275eebe272e7e22c987d4031340d996fd
Dominic Carretto <trimoxal@gmail.com>
2017-07-16 17:45:41 -0400

    feat(theme): Add boolean property to [mdc-theme-dark] directive
    Example: <button mdc-button [raised]="true" mdc-theme-dark="true">My button</button>

--- src/lib/theme/theme-dark.directive.ts
+++ src/lib/theme/theme-dark.directive.ts
@@ -1,11 +1,35 @@
 import {
   Directive,
-  HostBinding,
+  ElementRef,
+  Input,
+  OnChanges,
+  Renderer2,
+  SimpleChange,
 } from '@angular/core';
+import { toBoolean } from '../common/boolean-property';
 
 @Directive({
   selector: '[mdc-theme-dark]'
 })
-export class ThemeDark {
-  @HostBinding('class.mdc-theme--dark') isHostClass = true;
+export class ThemeDark implements OnChanges {
+  private nativeEl: ElementRef;
+  private _mdcThemeDark: boolean;
+
+  @Input('mdc-theme-dark') mdcThemeDark: boolean;
+
+  constructor(
+    private _renderer: Renderer2,
+    private _root: ElementRef) {
+    this.nativeEl = this._root.nativeElement;
+  }
+
+  ngOnChanges(changes: { [key: string]: SimpleChange }) {
+    let change = changes['mdcThemeDark'];
+
+    if (!toBoolean(change.currentValue)) {
+      this._renderer.removeClass(this.nativeEl, 'mdc-theme--dark');
+    } else {
+      this._renderer.addClass(this.nativeEl, 'mdc-theme--dark');
+    }
+  }
 }

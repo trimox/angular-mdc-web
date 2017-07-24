e440d7355c16d98024190030ed9c93bde8709974
Dominic Carretto <trimoxal@gmail.com>
2017-07-24 19:49:27 -0400

    feat(textfield): Implement MDC Text field boxes
    Text field boxes support all of the same features as normal textfields, including help text,
    validation, and dense UI.
    
    Closes #57

---
+++ src/lib/textfield/textfield-box.component.ts
@@ -0,0 +1,37 @@
+import {
+  Component,
+  HostBinding,
+  OnInit,
+} from '@angular/core';
+import { Ripple } from '.././ripple/ripple.directive';
+import { TextfieldComponent } from './textfield.component';
+
+@Component({
+  selector: 'mdc-textfield-box',
+  template:
+  `
+  <input #input class="mdc-textfield__input"
+  [type]="type"
+  [id]="id"
+  [attr.name]="name"
+  [(ngModel)]="value"
+  [tabindex]="tabindex"
+  [maxlength]="maxlength"
+  [disabled]="disabled"
+  [required]="required"
+  (focus)="onFocus($event)"
+  (keydown)="onKeyDown($event)"
+  (blur)="onBlur($event)"
+  (input)="onInput($event)" />
+  <label #inputlabel [attr.for]="id" class="mdc-textfield__label">{{label}}</label>
+  <div class="mdc-textfield__bottom-line"></div>
+  `,
+  providers: [Ripple]
+})
+export class TextfieldBoxComponent extends TextfieldComponent implements OnInit {
+  @HostBinding('class.mdc-textfield--box') isHostClass = true;
+
+  ngOnInit() {
+    this.ripple.init();
+  }
+}

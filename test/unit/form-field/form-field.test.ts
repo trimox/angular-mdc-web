8ab775468008691214f83f0afc7e76c6d15a9a0c
Dominic Carretto <trimox@users.noreply.github.com>
2017-09-24 20:18:14 -0400

    test(form-field): Add test coverage (#213)

---
+++ test/unit/form-field/form-field.test.ts
@@ -0,0 +1,80 @@
+import { Component, DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import {
+  MdcFormFieldDirective,
+  MdcFormFieldModule,
+  MdcCheckboxModule,
+  MdcRadioModule,
+  MdcSwitchModule
+} from '../../../src/lib/public_api';
+
+describe('MdcFormFieldDirective', () => {
+  let fixture: ComponentFixture<any>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        MdcFormFieldModule,
+        MdcCheckboxModule,
+        MdcRadioModule,
+        MdcSwitchModule
+      ],
+      declarations: [
+        SimpleTest,
+      ]
+    });
+    TestBed.compileComponents();
+  }));
+
+  describe('basic behaviors', () => {
+    let testDebugElement: DebugElement;
+    let testNativeElement: HTMLElement;
+    let testInstance: MdcFormFieldDirective;
+    let testComponent: SimpleTest;
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(SimpleTest);
+      fixture.detectChanges();
+
+      testDebugElement = fixture.debugElement.query(By.directive(MdcFormFieldDirective));
+      testNativeElement = testDebugElement.nativeElement;
+      testInstance = testDebugElement.componentInstance;
+      testComponent = fixture.debugElement.componentInstance;
+    });
+
+    it('#should have mdc-form-field by default', () => {
+      expect(testDebugElement.nativeElement.classList)
+        .toContain('mdc-form-field', 'Expected to have mdc-form-field');
+    });
+
+    it('#should contain mdc-form-field--align-end css class', () => {
+      testComponent.isAlignEnd = true;
+      fixture.detectChanges();
+      expect(testDebugElement.nativeElement.classList).toContain('mdc-form-field--align-end');
+    });
+  });
+});
+
+/** Simple component for testing. */
+@Component({
+  template:
+  `
+    <mdc-form-field [alignEnd]="isAlignEnd">
+      <mdc-checkbox></mdc-checkbox>
+      <label>My label</label>
+    </mdc-form-field>
+    <mdc-form-field>
+      <mdc-radio></mdc-radio>
+      <label>My label</label>
+    </mdc-form-field>
+    <mdc-form-field>
+      <mdc-switch></mdc-switch>
+      <label>My label</label>
+    </mdc-form-field>
+  `,
+})
+class SimpleTest {
+  isAlignEnd: boolean = false;
+}

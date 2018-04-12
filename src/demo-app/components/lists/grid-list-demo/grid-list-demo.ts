fa9c491499c1e7abb07770132b472f01aac2988b
Dominic Carretto <trimox@users.noreply.github.com>
2018-04-12 07:44:03 -0400

    docs(demo): Improve various documentation pages (#859)

--- src/demo-app/components/lists/grid-list-demo/grid-list-demo.ts
+++ src/demo-app/components/lists/grid-list-demo/grid-list-demo.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './grid-list-demo.html',
 })
 export class GridListDemo {
-  gridTiles = [
+  tiles = [
     { title: 'Single Very Long Grid Title', caption: 'Caption', icon: 'star_outline' },
     { title: 'Single Very Long Grid Title', caption: 'Caption', icon: 'star_outline' },
     { title: 'Single Very Long Grid Title', caption: 'Caption', icon: 'star_outline' },

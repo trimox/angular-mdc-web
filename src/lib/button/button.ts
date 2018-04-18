886fb5317690c2fd546d4a1c456238dbadc153c7
Dominic Carretto <trimox@users.noreply.github.com>
2018-04-18 16:28:00 -0400

    feat(button): New UX state methods (#878)
    - Adds `setDense(dense: boolean)`
    - Adds `setSecondary(secondary: boolean)`
    - Adds `setUnelevated(unelevated: boolean)`
    - Adds `setStroked(stroked: boolean)`
    - Adds `setIcon(icon: boolean)`
    - Adds `setDisabled(disabled: boolean)`
    - Minor code refactoring
    - Adds advanced button example
    - Adds advanced Sass custom color examples

--- src/lib/button/button.ts
+++ src/lib/button/button.ts
@@ -7,10 +7,8 @@ import {
   HostBinding,
   HostListener,
   Input,
-  OnChanges,
   OnDestroy,
   Renderer2,
-  SimpleChange,
   ViewEncapsulation,
 } from '@angular/core';
 import {
@@ -28,34 +26,69 @@ import { MdcIcon } from '@angular-mdc/web/icon';
   template: '<ng-content></ng-content>',
   providers: [
     MdcRipple,
-    EventRegistry,
+    EventRegistry
   ],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  preserveWhitespaces: false,
+  preserveWhitespaces: false
 })
-export class MdcButton implements AfterContentInit, OnChanges, OnDestroy {
-  private _disabled: boolean = false;
-  private _disableRipple: boolean = false;
-  private _icon: boolean = false;
+export class MdcButton implements AfterContentInit, OnDestroy {
+  @Input()
+  get raised(): boolean { return this._raised; }
+  set raised(value: boolean) {
+    this.setRaised(value);
+  }
+  protected _raised: boolean = false;
 
-  @Input() raised: boolean = false;
-  @Input() primary: boolean = false;
-  @Input() dense: boolean = false;
-  @Input() secondary: boolean = false;
-  @Input() unelevated: boolean = false;
-  @Input() stroked: boolean = false;
+  @Input()
+  get primary(): boolean { return this._primary; }
+  set primary(value: boolean) {
+    this.setPrimary(value);
+  }
+  protected _primary: boolean = false;
+
+  @Input()
+  get dense(): boolean { return this._dense; }
+  set dense(value: boolean) {
+    this.setDense(value);
+  }
+  protected _dense: boolean = false;
+
+  @Input()
+  get secondary(): boolean { return this._secondary; }
+  set secondary(value: boolean) {
+    this.setSecondary(value);
+  }
+  protected _secondary: boolean = false;
+
+  @Input()
+  get unelevated(): boolean { return this._unelevated; }
+  set unelevated(value: boolean) {
+    this.setUnelevated(value);
+  }
+  protected _unelevated: boolean = false;
+
+  @Input()
+  get stroked(): boolean { return this._stroked; }
+  set stroked(value: boolean) {
+    this.setStroked(value);
+  }
+  protected _stroked: boolean = false;
 
   @Input()
   get icon(): boolean { return this._icon; }
   set icon(value: boolean) {
-    this._icon = toBoolean(value);
+    this.setIcon(value);
   }
+  protected _icon: boolean = false;
+
   @Input()
   get disabled(): boolean { return this._disabled; }
   set disabled(value: boolean) {
-    this._disabled = toBoolean(value);
+    this.setDisabled(value);
   }
+  protected _disabled: boolean = false;
+
   @HostBinding('tabindex') get tabindex(): number {
     return this.disabled ? -1 : 0;
   }
@@ -88,27 +121,6 @@ export class MdcButton implements AfterContentInit, OnChanges, OnDestroy {
     public elementRef: ElementRef,
     public ripple: MdcRipple) { }
 
-  ngOnChanges(changes: { [key: string]: SimpleChange }): void {
-    const disabled = changes['disabled'];
-    const icon = changes['icon'];
-
-    if (disabled) {
-      if (disabled.currentValue) {
-        this.renderer.setAttribute(this._getHostElement(), 'disabled', 'true');
-        this.renderer.setAttribute(this._getHostElement(), 'aria-disabled', 'true');
-      } else {
-        this.renderer.removeAttribute(this._getHostElement(), 'disabled');
-        this.renderer.removeAttribute(this._getHostElement(), 'aria-disabled');
-      }
-    }
-
-    if (icon) {
-      if (icon.currentValue && this.buttonIcon) {
-        this.renderer.addClass(this.buttonIcon.elementRef.nativeElement, 'mdc-button__icon');
-      }
-    }
-  }
-
   ngAfterContentInit(): void {
     this.ripple.attachTo(this._getHostElement());
   }
@@ -117,12 +129,58 @@ export class MdcButton implements AfterContentInit, OnChanges, OnDestroy {
     this.ripple.destroy();
   }
 
+  setDisabled(disabled: boolean): void {
+    this._disabled = disabled;
+
+    if (disabled) {
+      this.renderer.setAttribute(this._getHostElement(), 'disabled', 'true');
+      this.renderer.setAttribute(this._getHostElement(), 'aria-disabled', 'true');
+    } else {
+      this.renderer.removeAttribute(this._getHostElement(), 'disabled');
+      this.renderer.removeAttribute(this._getHostElement(), 'aria-disabled');
+    }
+  }
+
+  setRaised(raised: boolean): void {
+    this._raised = raised;
+  }
+
+  setPrimary(primary: boolean): void {
+    this._primary = primary;
+  }
+
+  setDense(dense: boolean): void {
+    this._dense = dense;
+  }
+
+  setSecondary(secondary: boolean): void {
+    this._secondary = secondary;
+  }
+
+  setUnelevated(unelevated: boolean): void {
+    this._unelevated = unelevated;
+  }
+
+  setStroked(stroked: boolean): void {
+    this._stroked = stroked;
+  }
+
+  setIcon(icon: boolean): void {
+    this._icon = icon;
+
+    if (icon && this.buttonIcon) {
+      this.renderer.addClass(this.buttonIcon.elementRef.nativeElement, 'mdc-button__icon');
+    } else {
+      this.renderer.removeClass(this.buttonIcon.elementRef.nativeElement, 'mdc-button__icon');
+    }
+  }
+
   /** Focuses the button. */
   focus(): void {
     this._getHostElement().focus();
   }
 
-  _getHostElement() {
+  protected _getHostElement() {
     return this.elementRef.nativeElement;
   }
 
--- test/unit/button/button.test.ts
+++ test/unit/button/button.test.ts
@@ -183,6 +183,13 @@ describe('MdcButton', () => {
     it('#should have class mdc-button__icon by default', () => {
       expect(buttonDebugElement.nativeElement.classList.contains('mdc-button__icon')).toBe(true);
     });
+
+    it('#should not apply icon style', () => {
+      testComponent.icon = false;
+      fixture.detectChanges();
+
+      expect(buttonNativeElement.classList.contains('mdc-button__icon')).toBe(false);
+    });
   });
 });
 

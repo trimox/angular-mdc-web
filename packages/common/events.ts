6387747a6cea653ef70579a75fe37413d6b36742
Dominic Carretto <trimox@users.noreply.github.com>
2018-08-10 23:58:37 -0400

    chore: Remove unused events.ts (#1197)

--- packages/common/events.ts
+++
@@ -1,8 +0,0 @@
-/// Determines if the space key was pressed in a [KeyboardEvent].
-///
-/// Use this utility because `keyCode` is deprecated in Firefox (and doesn't
-/// work for space) and `key` is not yet implemented in Chrome.
-export function isSpaceKey(event: KeyboardEvent): boolean {
-  // NB: keyCode does not work on Firefox, returning `0` for the space key.
-  return event.keyCode !== 0 ? event.keyCode === 32 : event.key === ' ';
-}

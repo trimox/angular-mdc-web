7970289a00babe4495bd4e4003eabf142c08043c
Dominic Carretto <trimox@users.noreply.github.com>
2018-09-20 20:18:52 -0400

    refactor(top-app-bar): Use directive config + strongly-typed ElementRef (#1354)

--- packages/top-app-bar/top-app-bar.directives.ts
+++ packages/top-app-bar/top-app-bar.directives.ts
@@ -3,25 +3,31 @@ import {
   Component,
   Directive,
   ElementRef,
-  HostBinding,
   Input
 } from '@angular/core';
 import { MdcRipple } from '@angular-mdc/web/ripple';
 
 @Directive({
   selector: 'mdc-top-app-bar-row, [mdcTopAppBarRow]',
-  exportAs: 'mdcTopAppBarRow'
+  exportAs: 'mdcTopAppBarRow',
+  host: {
+    'class': 'mdc-top-app-bar__row'
+  }
 })
 export class MdcTopAppBarRow {
-  @HostBinding('class.mdc-top-app-bar__row') isHostClass = true;
-
-  constructor(public elementRef: ElementRef) { }
+  constructor(public elementRef: ElementRef<HTMLElement>) { }
 }
 
 @Component({
   moduleId: module.id,
   selector: 'mdc-top-app-bar-section, [mdcTopAppBarSection]',
   exportAs: 'mdcTopAppBarSection',
+  host: {
+    'role': 'toolbar',
+    'class': 'mdc-top-app-bar__section',
+    '[class.mdc-top-app-bar__section--align-start]': 'align === "start"',
+    '[class.mdc-top-app-bar__section--align-end]': 'align === "end"'
+  },
   template: `
   <ng-content></ng-content>
   <span class="mdc-top-app-bar__title" *ngIf="title">{{title}}</span>
@@ -32,29 +38,21 @@ export class MdcTopAppBarSection {
   @Input() title: string;
   @Input() align: string;
 
-  @HostBinding('class.mdc-top-app-bar__section') isHostClass = true;
-  @HostBinding('attr.role') role: string = 'toolbar';
-  @HostBinding('class.mdc-top-app-bar__section--align-start') get classAlignStart(): string {
-    return this.align === 'start' ? 'mdc-top-app-bar__section--align-start' : '';
-  }
-  @HostBinding('class.mdc-top-app-bar__section--align-end') get classAlignEnd(): string {
-    return this.align === 'end' ? 'mdc-top-app-bar__section--align-end' : '';
-  }
-
-  constructor(public elementRef: ElementRef) { }
+  constructor(public elementRef: ElementRef<HTMLElement>) { }
 }
 
 @Directive({
   selector: 'mdc-icon[mdcTopAppBarActionItem], [mdcTopAppBarActionItem], mdc-top-app-bar-action-item',
   exportAs: 'mdcTopAppBarActionItem',
+  host: {
+    'role': 'button',
+    'class': 'mdc-top-app-bar__action-item'
+  },
   providers: [MdcRipple]
 })
 export class MdcTopAppBarActionItem {
-  @HostBinding('attr.role') role: string = 'button';
-  @HostBinding('class.mdc-top-app-bar__action-item') isHostClass = true;
-
   constructor(
-    public elementRef: ElementRef,
+    public elementRef: ElementRef<HTMLElement>,
     private _ripple: MdcRipple) {
 
     _ripple.attachTo(this.elementRef.nativeElement, true);
@@ -64,14 +62,15 @@ export class MdcTopAppBarActionItem {
 @Directive({
   selector: 'mdc-icon[mdcTopAppBarNavIcon], [mdcTopAppBarNavIcon], mdc-icon[mdcTopAppBarNavigationIcon]',
   exportAs: 'mdcTopAppBarNavigationIcon',
+  host: {
+    'role': 'button',
+    'class': 'mdc-top-app-bar__navigation-icon'
+  },
   providers: [MdcRipple]
 })
 export class MdcTopAppBarNavigationIcon {
-  @HostBinding('attr.role') role: string = 'button';
-  @HostBinding('class.mdc-top-app-bar__navigation-icon') isHostClass = true;
-
   constructor(
-    public elementRef: ElementRef,
+    public elementRef: ElementRef<HTMLElement>,
     private _ripple: MdcRipple) {
 
     _ripple.attachTo(this.getHostElement(), true);
@@ -85,10 +84,11 @@ export class MdcTopAppBarNavigationIcon {
 
 @Directive({
   selector: 'mdc-top-app-bar-title, [mdcTopAppBarTitle]',
-  exportAs: 'mdcTopAppBarTitle'
+  exportAs: 'mdcTopAppBarTitle',
+  host: {
+    'class': 'mdc-top-app-bar__title'
+  },
 })
 export class MdcTopAppBarTitle {
-  @HostBinding('class.mdc-top-app-bar__title') isHostClass = true;
-
-  constructor(public elementRef: ElementRef) { }
+  constructor(public elementRef: ElementRef<HTMLElement>) { }
 }

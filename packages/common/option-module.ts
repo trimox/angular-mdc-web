4ae36a81d0b1f2a3cb5a06b03de9e276d6e56301
Dominic Carretto <trimox@users.noreply.github.com>
2018-09-26 06:06:15 -0400

    chore: Remove unneeded option module

--- packages/common/option-module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { MdcOption } from './option';
-
-@NgModule({
-  exports: [MdcOption],
-  declarations: [MdcOption]
-})
-export class MdcOptionModule { }

511087bc2dbd0a5223996e3dce61ae9ab3c4f196
Dominic Carretto <trimox@users.noreply.github.com>
2018-10-13 12:27:28 -0400

    fix(image-list): Remove HostBindings (#1437)

--- packages/image-list/image-list.ts
+++ packages/image-list/image-list.ts
@@ -4,7 +4,6 @@ import {
   Component,
   Directive,
   ElementRef,
-  HostBinding,
   Input,
   ViewEncapsulation
 } from '@angular/core';
@@ -12,11 +11,10 @@ import { toBoolean } from '@angular-mdc/web/common';
 
 @Directive({
   selector: '[mdcImageListItem], mdc-image-list-item',
-  exportAs: 'mdcImageListItem'
+  exportAs: 'mdcImageListItem',
+  host: { 'class': 'mdc-image-list__item' }
 })
 export class MdcImageListItem {
-  @HostBinding('class.mdc-image-list__item') isHostClass = true;
-
   constructor(public elementRef: ElementRef) { }
 }
 
@@ -37,31 +35,28 @@ export class MdcImageListImageAspect {
 
 @Directive({
   selector: '[mdcImageListImage], mdc-image-list-image',
-  exportAs: 'mdcImageListImage'
+  exportAs: 'mdcImageListImage',
+  host: { 'class': 'mdc-image-list__image' }
 })
 export class MdcImageListImage {
-  @HostBinding('class.mdc-image-list__image') isHostClass = true;
-
   constructor(public elementRef: ElementRef) { }
 }
 
 @Directive({
   selector: '[mdcImageListSupporting], mdc-image-list-supporting',
   exportAs: 'mdcImageListSupporting',
+  host: { 'class': 'mdc-image-list__supporting' }
 })
 export class MdcImageListSupporting {
-  @HostBinding('class.mdc-image-list__supporting') isHostClass = true;
-
   constructor(public elementRef: ElementRef) { }
 }
 
 @Directive({
   selector: '[mdcImageListLabel], mdc-image-list-label',
-  exportAs: 'mdcImageListLabel'
+  exportAs: 'mdcImageListLabel',
+  host: { 'class': 'mdc-image-list__label' }
 })
 export class MdcImageListLabel {
-  @HostBinding('class.mdc-image-list__label') isHostClass = true;
-
   constructor(public elementRef: ElementRef) { }
 }
 
@@ -69,6 +64,11 @@ export class MdcImageListLabel {
   moduleId: module.id,
   selector: '[mdc-image-list], mdc-image-list',
   exportAs: 'mdcImageList',
+  host: {
+    'class': 'mdc-image-list',
+    '[class.mdc-image-list--masonry]': 'masonry',
+    '[class.mdc-image-list--with-text-protection]': 'textProtection'
+  },
   template: '<ng-content></ng-content>',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None
@@ -77,36 +77,18 @@ export class MdcImageList {
   @Input()
   get masonry(): boolean { return this._masonry; }
   set masonry(value: boolean) {
-    this.setMasonry(value);
+    this._masonry = toBoolean(value);
   }
-  protected _masonry: boolean;
+  private _masonry: boolean;
 
   @Input()
   get textProtection(): boolean { return this._textProtection; }
   set textProtection(value: boolean) {
-    this.setTextProtection(value);
+    this._textProtection = toBoolean(value);
   }
-  protected _textProtection: boolean;
-
-  @HostBinding('class.mdc-image-list') isHostClass = true;
-  @HostBinding('class.mdc-image-list--masonry') get classMasonry(): string {
-    return this.masonry ? 'mdc-image-list--masonry' : '';
-  }
-  @HostBinding('class.mdc-image-list--with-text-protection') get classTextProtection(): string {
-    return this.textProtection ? 'mdc-image-list--with-text-protection' : '';
-  }
+  private _textProtection: boolean;
 
   constructor(
     private _changeDetectorRef: ChangeDetectorRef,
     public elementRef: ElementRef) { }
-
-  setMasonry(masonry: boolean): void {
-    this._masonry = toBoolean(masonry);
-    this._changeDetectorRef.markForCheck();
-  }
-
-  setTextProtection(textProtection: boolean): void {
-    this._textProtection = toBoolean(textProtection);
-    this._changeDetectorRef.markForCheck();
-  }
 }

acdf1cbe5d4b8dbbe8dd1e121e1d65f2c8c41190
Dominic Carretto <trimox@users.noreply.github.com>
2018-11-01 20:49:18 -0400

    feat(slider): Expose sliderThumb elementRef (#1516)
    Allow `_sliderThumb` to be accessed.
    
    ```
    @ViewChild('sliderThumb') _sliderThumb: ElementRef<HTMLElement>;
    ```
    
    reference: #1510

--- packages/slider/slider.ts
+++ packages/slider/slider.ts
@@ -14,6 +14,7 @@ import {
   ViewEncapsulation
 } from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
 import { toNumber, toBoolean, Platform } from '@angular-mdc/web/common';
 
 import { strings } from '@material/slider/constants';
@@ -51,7 +52,7 @@ export class MdcSliderChange {
       <div *ngIf="discrete" class="mdc-slider__pin">
         <span #pin class="mdc-slider__pin-value-marker"></span>
       </div>
-      <svg
+      <svg #sliderThumb
         class="mdc-slider__thumb"
         width="21" height="21"
         focusable="false">
@@ -137,9 +138,10 @@ export class MdcSlider implements AfterViewInit, OnDestroy, ControlValueAccessor
   @Output() readonly input: EventEmitter<MdcSliderChange> = new EventEmitter<MdcSliderChange>();
 
   @ViewChild('thumbcontainer') thumbContainer: ElementRef<HTMLElement>;
+  @ViewChild('sliderThumb') _sliderThumb: ElementRef<HTMLElement>;
   @ViewChild('track') track: ElementRef<HTMLElement>;
   @ViewChild('pin') pinValueMarker: ElementRef;
-  @ViewChild('markercontainer') trackMarkerContainer: ElementRef;
+  @ViewChild('markercontainer') trackMarkerContainer: ElementRef<HTMLElement>;
 
   /** View -> model callback called when value changes */
   _onChange: (value: any) => void = () => { };

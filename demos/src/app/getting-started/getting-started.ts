1a900bc2a880bd6882849c2d35e3337b1cde8e69
dgoudie <34222208+dgoudie@users.noreply.github.com>
2019-06-15 16:45:53 -0500

    demos: Update getting-started.ts (#1908)

--- demos/src/app/getting-started/getting-started.ts
+++ demos/src/app/getting-started/getting-started.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 export class GettingStarted {
   npmCmd = `npm i @angular-mdc/web`;
   yarnCmd = `yarn add @angular-mdc/web`;
-  materialIconsLink = `<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"`;
+  materialIconsLink = `<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">`;
   robotoLink = `<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">`;
   sassHelp = `$mdc-theme-primary: #1565c0; // primary color
 $mdc-theme-secondary: #388e3c; // secondary color

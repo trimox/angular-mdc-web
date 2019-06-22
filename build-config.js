d9b47ec3d80bd0d8420af1979c00e8502ffe941d
Dominic Carretto <trimox@users.noreply.github.com>
2019-06-22 18:00:34 -0400

    build: Add rule to enforce static queries (#1928)
    Adds a simplified tslint rule to enforce that we've explicitly set the `static` flag on relevant queries. We don't need the more advanced functionality from the one in angular/angular, because we don't have to figure out which value is correct, only that it is set.
    
    Sync with [@angular/components](https://github.com/angular/components)

--- build-config.js
+++ build-config.js
@@ -18,7 +18,7 @@ const buildVersion = packageJson.version;
 /** License that will be placed inside of all created bundles. */
 const buildLicense = `/**
  * @license
- * Copyright (c) 2018 Dominic Carretto
+ * Copyright (c) 2019 Dominic Carretto
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://github.com/trimox/angular-mdc-web/blob/master/LICENSE

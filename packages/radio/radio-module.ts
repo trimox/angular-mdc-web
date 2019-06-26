dd3c93080e3e1223b21b07f1f7147a284edd0baa
Dominic Carretto <trimox@users.noreply.github.com>
2019-06-26 18:58:59 -0400

    chore: Bump dependencies (#1940)

--- packages/radio/radio-module.ts
+++ packages/radio/radio-module.ts
@@ -1,17 +1,11 @@
 import {NgModule} from '@angular/core';
 
 import {MdcFormFieldModule} from '@angular-mdc/web/form-field';
-import {MdcRadioGroup, MdcRadio} from './radio';
+import {MdcRadio, MdcRadioGroup} from './radio';
 
 @NgModule({
   imports: [MdcFormFieldModule],
-  exports: [
-    MdcRadio,
-    MdcRadioGroup
-  ],
-  declarations: [
-    MdcRadio,
-    MdcRadioGroup
-  ]
+  exports: [MdcRadioGroup, MdcRadio],
+  declarations: [MdcRadioGroup, MdcRadio]
 })
 export class MdcRadioModule { }

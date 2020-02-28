63a01113c9bf705ded724e91a28513ade4acf357
Dominic Carretto <trimoxal@gmail.com>
2020-02-27 22:10:14 -0500

    feat(samples): Starter app

---
+++ examples/example-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MaterialModule} from './material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
---
+++ examples/example-app/src/app/material.module.ts
@@ -0,0 +1,59 @@
+import {NgModule} from '@angular/core';
+import {
+  MdcButtonModule,
+  MdcCardModule,
+  MdcCheckboxModule,
+  MdcChipsModule,
+  MDCDataTableModule,
+  MdcDialogModule,
+  MdcDrawerModule,
+  MdcElevationModule,
+  MdcFabModule,
+  MdcIconButtonModule,
+  MdcIconModule,
+  MdcImageListModule,
+  MdcLinearProgressModule,
+  MdcListModule,
+  MdcMenuModule,
+  MdcRadioModule,
+  MdcRippleModule,
+  MdcSelectModule,
+  MdcSliderModule,
+  MdcSnackbarModule,
+  MdcSwitchModule,
+  MdcTabBarModule,
+  MdcTextFieldModule,
+  MdcTopAppBarModule,
+  MdcTypographyModule
+} from '@angular-mdc/web';
+
+@NgModule({
+  exports: [
+    MdcButtonModule,
+    MdcCardModule,
+    MdcCheckboxModule,
+    MdcChipsModule,
+    MDCDataTableModule,
+    MdcDialogModule,
+    MdcDrawerModule,
+    MdcElevationModule,
+    MdcFabModule,
+    MdcIconButtonModule,
+    MdcIconModule,
+    MdcImageListModule,
+    MdcLinearProgressModule,
+    MdcListModule,
+    MdcMenuModule,
+    MdcRadioModule,
+    MdcRippleModule,
+    MdcSelectModule,
+    MdcSliderModule,
+    MdcSnackbarModule,
+    MdcSwitchModule,
+    MdcTabBarModule,
+    MdcTextFieldModule,
+    MdcTopAppBarModule,
+    MdcTypographyModule
+  ]
+})
+export class MaterialModule {}

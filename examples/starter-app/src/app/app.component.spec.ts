0de291db1d735f86dcea48e932c74cff90c17555
Dominic Carretto <trimoxal@gmail.com>
2020-02-28 06:57:08 -0500

    WIP - starter app

--- examples/starter-app/src/app/app.component.spec.ts
+++ examples/starter-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'example-app'`, () => {
+  it(`should have as title 'starter-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('example-app');
+    expect(app.title).toEqual('starter-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('example-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('starter-app app is running!');
   });
 });

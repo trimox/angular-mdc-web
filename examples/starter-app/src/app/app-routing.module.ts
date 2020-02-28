7425c61a8dd73be1e573349ec0a98030f52f9052
Dominic Carretto <trimoxal@gmail.com>
2020-02-28 07:55:07 -0500

    WIP - starter app

--- examples/starter-app/src/app/app-routing.module.ts
+++ examples/starter-app/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 const routes: Routes = [];
 
@@ -8,4 +7,4 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
